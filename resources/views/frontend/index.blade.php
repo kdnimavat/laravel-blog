3a7b5cf88cc8de70958f25b961bd95d4bf8b396a
henilAsTech <henil.astech@gmail.com>
2022-06-08 17:52:49 +0530

    updated

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -6,12 +6,13 @@
             <div class="row">
                 <div class="col-md-12">
                     <h4>All Categories List</h4>
+                    <hr style="color: black">
                     <div class="inderline"></div>
 
                 </div>
                 @foreach ($all_categories as $all_cateitem)
                     <div class="col-md-3">
-                        <div class="card card-body mb-3">
+                        <div class="card card-body mb-3" style="background-color: lightgray">
                             <a href="{{ url('tutorial/' . $all_cateitem->slug) }}" class="text-decoration-none">
                                 <h5 class="text-dark mb-8">{{ $all_cateitem->name }}</h5>
                             </a>
